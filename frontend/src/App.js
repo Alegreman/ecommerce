6715846b39d15dee297d783784512cf573cef6aa
alegreman <jorgealegriadev@gmail.com>
2020-10-27 11:40:55 -0700

    rating component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Container } from 'react-bootstrap'
 import Header from './components/Header';
 import Footer from './components/Footer';
+import HomeScreen from './Screens/HomeScreen';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <main className="py-3">
         <Container>
+          <HomeScreen />
           <h1>Welcome to Shop</h1>
         </Container>
       </main>
