cadd0ccdd17e405249fd798fd04451191b4537c2
alegreman <jorgealegriadev@gmail.com>
2020-10-27 21:02:34 -0700

    single product component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomeScreen from './Screens/HomeScreen';
+import ProductScreen from './Screens/ProductScreen';
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
       <main className="py-3">
         <Container>
-          <HomeScreen />
-          <h1>Welcome to Shop</h1>
+          <Route path='/' component={HomeScreen} exact />
+          <Route path='/product/:id' component={ProductScreen} />
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 }
 
